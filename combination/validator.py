a0422b465f614df3201e207b237ce4b6beec7bdf
Martin Scharm <martin@binfalse.de>
2015-10-14 16:07:17 -0600

    removes time consuming checks during validation
    thanks to frank bergmann for sharing his expertise :)

--- combination/validator.py
+++ combination/validator.py
@@ -4,8 +4,14 @@ from libsbml import *
 reader = SBMLReader()
 #print "loading model"
 document = reader.readSBML(sys.argv[1])
-print document.getNumErrors()
-#print "checking for consistency"
-print document.checkConsistency()
+nErr = document.getNumErrors ()
+if (nErr > 0):
+    print nErr
+else:
+    document.setConsistencyChecks(LIBSBML_CAT_UNITS_CONSISTENCY, False)
+    document.setConsistencyChecks(LIBSBML_CAT_MODELING_PRACTICE, False)
+    document.setConsistencyChecks(LIBSBML_CAT_MATHML_CONSISTENCY, False)
+    #print "checking for consistency"
+    print document.checkConsistency()
 
 
