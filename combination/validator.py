9794305de843f2c00a44c9bed415efa69d7eec1f
Martin Scharm <martin@binfalse.de>
2015-10-14 09:54:03 -0600

    updates the validator
    now also checks for consistency using the libsbml
    
    thanks to lucian and mike for the hint

--- combination/validator.py
+++ combination/validator.py
@@ -2,7 +2,10 @@ import sys
 from libsbml import *
 
 reader = SBMLReader()
+#print "loading model"
 document = reader.readSBML(sys.argv[1])
 print document.getNumErrors()
+#print "checking for consistency"
+print document.checkConsistency()
 
 
